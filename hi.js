672cd40bf54c697bc9aa1a0de8d7aee7e0634142
Crystaaal99 <126136030+Crystaaal99@users.noreply.github.com>
2023-04-17 01:30:59 -0500

    new commit

---
+++ hi.js
@@ -0,0 +1,5 @@
+function hi(){
+    var firstName = prompt("Enter your First Name")
+    var lastName = prompt("Enter your Last Name")
+    alert("Hi! " + firstName + " " + lastName + "," + "Welcome to my page!")
+}
