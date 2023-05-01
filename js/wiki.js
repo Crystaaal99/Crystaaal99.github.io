1e52bfdcfc71e68e7786fc0f3346cc910ff220e2
Crystaaal99 <126136030+Crystaaal99@users.noreply.github.com>
2023-04-30 20:53:59 -0500

    js

--- js/wiki.js
+++ js/wiki.js
@@ -12,15 +12,23 @@ function wikiAPI(){
         console.log(wikiObject);
         console.log(wikiObject.query.pages);
         var pages = wikiObject.query.pages;
-        for (var i in pages) {
-            var newDiv = document.createElement('div')
-            var newhref = document.createElement('a')
-            newhref.setAttribute('href', 'https://en.wikipedia.org/?curid='+ pages[i].pageid);
-            newDiv.setAttribute('class', 'row h4');
-            newDiv.setAttribute('id', 'div'+ i);
-            document.getElementById('wiki').appendChild(newDiv);
-            document.getElementById('div'+i).appendChild(newhref);
-            newhref.innerText = pages[i].title;
+        // for (i in pages) {
+        //     var newDiv = document.createElement('div')
+        //     var newhref = document.createElement('a')
+        //     newhref.setAttribute('href', 'https://en.wikipedia.org/?curid='+ pages[i].pageid);
+        //     newDiv.setAttribute('class', 'row h4');
+        //     newDiv.setAttribute('id', 'div'+ i);
+        //     document.getElementById('wiki').appendChild(newDiv);
+        //     document.getElementById('div'+i).appendChild(newhref);
+        //     newhref.innerText = pages[i].title;
+        // }
+        for (i in pages){
+            var pageURL = 'https://en.wikipedia.org/?curid='
+            var newAnchor = document.createElement('a')
+            newAnchor.href = pageURL+pages[i].pageid;
+            newAnchor.className = 'd-block';
+            newAnchor.innerText = pages[i].title;
+            document.getElementById('wiki').appendChild(newAnchor);
         }
     }
 
@@ -28,5 +36,7 @@ function wikiAPI(){
     connect.send();
 
     // superchallenge
+    
+
 
 }
